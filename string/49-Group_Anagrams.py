cb0dbc2d78459df2e5105c17621e11640b7e67ab
fhong <hongfu1220@gmail.com>
2021-06-24 12:50:21 -0700

    practice analyze space and time complexity

--- string/49-Group_Anagrams.py
+++ string/49-Group_Anagrams.py
@@ -1,19 +1,27 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # edge case: if len(strs) <= 1 -> return [strs]
+        # Create a dictionay, store the tuple of sorted string as key, and the index as value
+        # If key is in the dictionary, append string into the result array at value
+        # If it's not in the dictionary, append a new array in result and update the index
+        
+    
         if len(strs) <= 1: return [strs]
         
-        dic = {}
+        dic = collections.defaultdict(list)
         for s in strs:
-            sort_s = "".join(sorted(s))
-            if sort_s in dic:
-                dic[sort_s].append(s)
-            else:
-                dic[sort_s] = [s]
-
+            dic[tuple(sorted(s))].append(s)
+        
         return dic.values()
     
-
+    
+# Runtime: 84 ms, faster than 99.01% of Python3 online submissions for Group Anagrams.
+# Memory Usage: 17.9 MB, less than 44.71% of Python3 online submissions for Group Anagrams.
 '''
-Runtime: 80 ms, faster than 99.76% of Python3 online submissions for Group Anagrams.
-Memory Usage: 17.7 MB, less than 62.31% of Python3 online submissions for Group Anagrams.
+Time complexity:
+    N is length of strs. K is length of every string. Sort string is Klong(K).
+    Total = O(NKlogK).
+Space complexity:
+    dic size = N * K
+    Total = O(NK)
 '''
